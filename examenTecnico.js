5c29d55a5b935f2022eca751e512fdc363462a6d
Pedro Tanamachi <pedroeduardotanamachigarza@gmail.com>
2022-06-09 16:58:16 -0600

    mas cambios

--- examenTecnico.js
+++ examenTecnico.js
@@ -29,9 +29,49 @@ function contarPalabras(cadena, palabra) {
     var arr = cadena.split(' ');
     var conteo = 0;
     for (let i = 0; i < arr.length; i++) {
-        if(arr[i].toLowerCase() == palabra){
+        if (arr[i].toLowerCase() == palabra) {
             conteo++
         }
     }
-    console.log("Numero de veces que se repite la palabra",conteo);
+    console.log("Numero de veces que se repite la palabra", conteo);
+}
+
+//Función 3
+let arr = [0, null, undefined, 1, 2, 3, 4, 5, 6, 7, null, undefined, 0, false, false];
+limpiar(arr);
+
+function limpiar(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == null || arr[i] == 0 || arr[i] == false || arr[i] == undefined) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    console.log("Arreglo limpio:", arr);
+}
+
+//Funcion 4
+let arr2 = [1, 4, 7, 8, 4545, 76, 85, 53, 472, 2];
+valorMaxMin(arr2);
+
+function valorMaxMin(arr) {
+    var min = Math.min(...arr);
+    var max = Math.max(...arr);
+
+    console.log("Valor Maximo:", max);
+    console.log("Valor Minimo:", min);
+
+}
+
+//Funcion 5
+let arrMulti = [1, 2, 3, 4, 5, [3, [3, 7, [9, 10, 12, 34]]]];
+flatten(arrMulti);
+function flatten(arr){
+    var arrM = arr.toString();
+    var newArr = arrM.split(",");
+    var lastArr = [];
+    for(let i = 0;i<newArr.length;i++){
+        lastArr.push(parseInt(newArr[i]))
+    }
+    console.log("Resultado: \n",lastArr);
 }
