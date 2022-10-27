0d2f97f6a4e35fe60600136d6c3994638f0e89fc
Pedro Tanamachi <pedroeduardotanamachigarza@gmail.com>
2022-10-27 12:45:50 -0600

    Se agregan ejercicios con funciones que salieron en ECMA7

---
+++ EjerciciosJS/es7/00-exponenciales.js
@@ -0,0 +1,3 @@
+const data = 3 ** 4; //Esto hace la potencia del numero 3 al 4 de manera nativa sin necesidad de usar el Math.pow.
+
+console.log(data);
---
+++ EjerciciosJS/es7/01-arrayIncludes.js
@@ -0,0 +1,4 @@
+let numbers = [1, 2, 3, 54, 2, 3, 2, 5, 90];
+
+//Verifica si el valor dado dentro del inludes se encuentra dentro del array en cuestion, funciona tambien con strings y es sensible a las mayusculas
+console.log(numbers.includes(54));
