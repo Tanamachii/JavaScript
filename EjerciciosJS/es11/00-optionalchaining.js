a6dc102e50897a798f7b13c9a55935974da67b98
Pedro Tanamachi <pedroeduardotanamachigarza@gmail.com>
2022-11-02 11:54:00 -0700

    Se agregan ejercicios de es11

---
+++ EjerciciosJS/es11/00-optionalchaining.js
@@ -0,0 +1,10 @@
+const users = {
+  tana: {
+    country: "Mx",
+  },
+  gil: {
+    country: "US",
+  },
+};
+
+console.log(users.tanamachi?.country);
---
+++ EjerciciosJS/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const number = 909209309123412342342302930139231031293n;
+const anothernumber = BigInt(198230910391340394);
+
+console.log(number + anothernumber);
